6433f7ec98002c390d8f2132368c4835720d5f39
Rob Cole <robert.jcb.cole@gmail.com>
2015-04-04 21:23:52 +0000

    complete mocking with rspec checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,8 @@ class Post < ActiveRecord::Base
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-#  validates :topic, presence: true
-#  validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
   
   def up_votes
     votes.where(value: 1).count
@@ -41,7 +41,9 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
   
-  after_create :create_vote
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
   
   private
   
@@ -50,10 +52,6 @@ class Post < ActiveRecord::Base
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
-  end
-  
-  def create_vote
-    user.votes.create(value: 1, post: self)
   end
   
 end
