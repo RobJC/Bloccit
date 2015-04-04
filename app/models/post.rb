0e3fcc57b3338c3385d2850472c9904266838abf
Rob Cole <robert.jcb.cole@gmail.com>
2015-04-04 18:46:04 +0000

    complete voting checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,30 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
   has_one :summary
     mount_uploader :image, ImageUploader
 
   
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+#  validates :topic, presence: true
+#  validates :user, presence: true
+  
+  def up_votes
+    votes.where(value: 1).count
+  end
+  
+  def down_votes
+    votes.where(value: -1).count
+  end
+  
+  def points
+    votes.sum :value
+  end
   
   def markdown_title
     render_as_markdown(title)
@@ -19,6 +32,13 @@ class Post < ActiveRecord::Base
   
   def markdown_body
     render_as_markdown(body)
+  end
+  
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
+    
+    update_attribute(:rank, new_rank)
   end
   
   private
@@ -29,5 +49,7 @@ class Post < ActiveRecord::Base
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+  
+
   
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,23 +2,10 @@ require 'rails_helper'
 
 describe Vote do 
   describe "validations" do
-    
     describe "value validation" do
       it "allows only -1 or 1 as values" do
-        @vote = Vote.create(value: 2)
-        expect(@vote.create?).to eq(false)
-        
-        @vote = Vote.create(value: 1)
-        expect(@vote.create?).to eq(true)
-        
-        @vote = Vote.create(value: 0)
-        expect(@vote.create?).to eq(false)
-        
-        @vote = Vote.create(value: -1)
-        expect(@vote.create?).to eq(true)
-        
-        @vote = Vote.create(value: -2)
-        expect(@vote.create?).to eq(false)
+        @invalid_vote = Vote.create(value: 2)
+        expect(@invalid_vote.valid?).to eq(false)
       end
     end
   end
